969c6c79edc3807f1d81921437804aadf7756356
espoir habimfura <ehabimfura@users.noreply.github.com>
2026-02-23 02:30:03 +0200

    debug

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """Prints the titles of the first 10 hot posts listed for a given subreddit."""
-__author__ = "espoir habimfura"
 import requests
 
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "MyBot/0.1"}
+    """Print the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "linux:1-top_ten:v1.0 (by /u/espoir)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
